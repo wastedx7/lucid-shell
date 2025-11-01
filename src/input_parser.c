27e9af41c6922fdffb3278f6c62c34288ff01352
Anugrah Umarvanshi <wastedx7@gmail.com>
2025-11-01 20:58:39 +0530

    --

--- src/input_parser.c
+++ src/input_parser.c
@@ -6,6 +6,7 @@ char** input_parser(char* input)
     size_t buffer_size = MAX_INPUT;
     char** tokens = malloc(buffer_size * sizeof(char*));
     char* token = NULL;
+    size_t position = 0;
     size_t token_length = 0;
 
     if (!tokens){
@@ -14,7 +15,13 @@ char** input_parser(char* input)
     }
 
     for(size_t i=0; input[i]; i++){
+        token = &input[i];
+        while(input[i] && input[i] != ' '){
+            token_length++;
+            i++;
+        }
         
+        tokens[position] = malloc((token_length + 1) * sizeof(char));
     }
 
     return tokens;
